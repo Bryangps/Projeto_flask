b2e81dff5ef38108455f185c577764d4219758d9
bryan <bryan.gps@gmail.com>
2024-03-17 10:22:51 -0300

    criando as tabelas do banco de dados

--- comunidadeimpressionadora/__init__.py
+++ comunidadeimpressionadora/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
+import sqlalchemy
 
 #app é uma instacia da classe Flask
 app = Flask(__name__) #coloca o site no ar
@@ -20,5 +21,16 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'alert-info'
+
+
+from comunidadeimpressionadora import models
+engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+inspector = sqlalchemy.inspect(engine)
+if not inspector.has_table('usuario'):
+    database.drop_all()
+    database.create_all()
+    print('Base de Dados criado')
+else:
+    print('Base de dados já existente')
 
 from comunidadeimpressionadora import routes
